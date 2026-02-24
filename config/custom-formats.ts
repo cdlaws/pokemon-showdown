96b8c78ad21e1b0c618342a4b9b43bfa9ef42422
cdlaws <archedpvpz@gmail.com>
2026-02-24 10:21:20 -0500

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -1,13 +1,1 @@
-export const Formats: FormatList = [
-	{
-		section: "Draft",
-		column: 1,
-	},
-	{
-		name: "[ZA] NatDex Draft",
-		mod: 'gen9legendsou',
-		searchShow: false,
-		teraPreviewDefault: true,
-		ruleset: ['Standard Draft', '+Unobtainable', '+Past', 'Min Source Gen = 1'],
-	},
-];
+
